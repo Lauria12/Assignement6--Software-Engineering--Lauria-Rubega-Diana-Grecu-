643b487bca3aedfd13a34ba7e2b6f1314a501539
Lauria Rubega <112666834+Lauria12@users.noreply.github.com>
2023-05-27 21:06:50 +0200

    controllers params

--- src/main/java/com/example/assignement6sebook/InventoryController.java
+++ src/main/java/com/example/assignement6sebook/InventoryController.java
@@ -13,21 +13,21 @@ public class InventoryController
     private Inventory inventory;
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void addBook(Book book, int quantity) {
+    public void addBook(@RequestParam(value = "book")Book book, @RequestParam(value = "quantity")int quantity) {
 
         inventory.addBook(book, quantity);
 
     }
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void updateBookQuantity(Book book, int newQuantity) {
+    public void updateBookQuantity(@RequestParam(value = "book")Book book, @RequestParam(value = "newquantity")int newQuantity) {
 
         inventory.updateBookQuantity(book, newQuantity);
 
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public int getBookQuantity(Book book) {
+    public int getBookQuantity(@RequestParam(value = "book")Book book) {
 
         return getBookQuantity(book);
 
--- src/main/java/com/example/assignement6sebook/OrderController.java
+++ src/main/java/com/example/assignement6sebook/OrderController.java
@@ -3,6 +3,7 @@ package com.example.assignement6sebook;
 import com.example.assignement6sebook.Order.Order;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Date;
 
@@ -10,35 +11,35 @@ public class OrderController {
 
     private Order order;
 
-    public void setBookIdentifier(String bookIdentifier) {
+    public void setBookIdentifier(@RequestParam(value = "identifier") String bookIdentifier) {
 
         order.setBookIdentifier(bookIdentifier);
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping
     public String getBookIdentifier() {
 
         return getBookIdentifier();
     }
 
-    public void setQuantity(int quantity) {
+    public void setQuantity(@RequestParam(value = "quantity") int quantity) {
 
         order.setQuantity(quantity);
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping
     public int getQuantity() {
 
         return getQuantity();
     }
 
 
-    public void setOrderDate(Date date) {
+    public void setOrderDate(@RequestParam(value = "date")Date date) {
 
         order.setOrderDate(date);
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping
     public Date getOrderDate() {
 
         return getOrderDate();
